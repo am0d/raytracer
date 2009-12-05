9341f4a97e1cb0ebc94d9b233d26c7560e0e2468
a_m0d <damien.schoof@gmail.com>
2009-12-05 17:22:38 +0800

    Fixed whitespace in raytracer.hpp

--- raytracer.hpp
+++ raytracer.hpp
@@ -1,7 +1,7 @@
 /* Author: a_m0d <a_m0d@hotmail.com>
- * Date: June 2008
- * License: GPL version 2.0 or later
- */
+* Date: June 2008
+* License: GPL version 2.0 or later
+*/
 
 #ifndef raytracer_hpp
 #define raytracer_hpp
@@ -12,36 +12,37 @@
 #include "math/ray3d.hpp"
 
 class RayTracer {
-	public:
-		RayTracer ();
-		RayTracer (int left, int right, int top, int bottom);
-		RayTracer (const RayTracer& other);
+    public:
+    RayTracer ();
+    RayTracer (int left, int right, int top, int bottom);
+    RayTracer (const RayTracer& other);
 
-		~RayTracer ();
+    ~RayTracer ();
 
-		void initScene ();
-		bool render ();
+    void initScene ();
+    bool render ();
 
-		Bitmap* getBitmap ();
+    Bitmap* getBitmap ();
 
-		bool done ();
+    bool done ();
 
-	private:
+    private:
 
-		Color traceRay (Ray3D ray, float minDistance);
+    Color traceRay (Ray3D ray, float minDistance);
 
-		int _left, _right, _top, _bottom;	//holds the co-ords of our current viewport
 
-		float _deltaX, _deltaY;	//holds the amount to move our rays by
-		float _sigmaX, _sigmaY;	//holds the current x and y of the ray
-		int _curScanLine;	//holds our position on the bitmap
+    int _left, _right, _top, _bottom;	//holds the co-ords of our current viewport
 
-		bool _done;		//are we finished this render yet?
+    float _deltaX, _deltaY;	//holds the amount to move our rays by
+    float _sigmaX, _sigmaY;	//holds the current x and y of the ray
+    int _curScanLine;	//holds our position on the bitmap
 
-		Bitmap* _bitmap;	//holds our render surface
+    bool _done;		//are we finished this render yet?
 
-		Scene* _scene;	//list of the spheres in the scene
-					//TODO: Change this to a list of primitives!
+    Bitmap* _bitmap;	//holds our render surface
+
+    Scene* _scene;	//list of the spheres in the scene
+    //TODO: Change this to a list of primitives!
 };
 
 #endif
