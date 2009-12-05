8b329dca71deea1917a46c0fb6eb88dc2ccf047c
a_m0d <damien.schoof@gmail.com>
2009-12-05 17:27:03 +0800

    Fixed buildit file to set the compiler flags correctly

--- rt.py
+++ rt.py
@@ -3,7 +3,7 @@ from buildit import CXX
 
 raytracer = Unix("raytracer")
 raytracer.compiler = CXX()
-raytracer.compiler.add_flags(['-pedantic', '-Wall', '-ansi',
+raytracer.add_flag(['-pedantic', '-Wall', '-ansi',
                               '-Warray-bounds',
                               '-Wclobbered', '-Wconversion', '-Wdeprecated',
                               '-Weffc++', '-Wenum-compare', '-Wextra',
