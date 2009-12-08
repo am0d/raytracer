7da1a27b0b394aa001b8d2f377aafba1fea06cff
a_m0d <damien.schoof@gmail.com>
2009-12-08 23:01:51 +0800

    Fixed formatting in primitive.{cxx, hpp}

--- primitives/primitive.cxx
+++ primitives/primitive.cxx
@@ -1,18 +1,18 @@
 #include "../header.hpp"
 
 Primitive::Primitive () {
-	_color = Color ();
-	_isLight = false;
+    _color = Color ();
+    _isLight = false;
 }
 
 Primitive::Primitive (Color color, bool isLight) {
-	_color = color;
-	_isLight = isLight;
+    _color = color;
+    _isLight = isLight;
 }
 
 Primitive::Primitive (const Primitive& other) {
-	_color = other._color;
-	_isLight = other._isLight;
+    _color = other._color;
+    _isLight = other._isLight;
 }
 
 Primitive::~Primitive () {
@@ -20,21 +20,21 @@ Primitive::~Primitive () {
 }
 
 Color Primitive::getColorAt (Vector3 position) {
-	return _color;
+    return _color;
 }
 
 bool Primitive::getIsLight () {
-	return _isLight;
+    return _isLight;
 }
 
 void Primitive::setIsLight (bool isLight) {
-	_isLight = isLight;
+    _isLight = isLight;
 }
 
 void Primitive::setColor (Color color) {
-	_color = color;
+    _color = color;
 }
 
 Color Primitive::getColor () {
-	return _color;
+    return _color;
 }
--- primitives/primitive.hpp
+++ primitives/primitive.hpp
@@ -1,7 +1,7 @@
 /* Author: a_m0d <a_m0d@hotmail.com>
- * Date: May 2008
- * License: GPL version 2.0 or later
- */
+* Date: May 2008
+* License: GPL version 2.0 or later
+*/
 
 #ifndef primitive_hpp
 #define primitive_hpp
@@ -11,27 +11,27 @@
 #include "../math/ray3d.hpp"
 
 class Primitive {
-	public:
-		Primitive ();
-		Primitive (Color color, bool isLight = false);
-		Primitive (const Primitive& other);
+    public:
+    Primitive ();
+    Primitive (Color color, bool isLight = false);
+    Primitive (const Primitive& other);
 
-		virtual ~Primitive ();
+    virtual ~Primitive ();
 
-		virtual Vector3 getNormalAt (Vector3 position) = 0;
-		virtual Vector3 getOrigin () = 0;
-		virtual Color getColorAt (Vector3 position);
-		virtual float findIntersectionWith (Ray3D ray, float minDistance) = 0;
+    virtual Vector3 getNormalAt (Vector3 position) = 0;
+    virtual Vector3 getOrigin () = 0;
+    virtual Color getColorAt (Vector3 position);
+    virtual float findIntersectionWith (Ray3D ray, float minDistance) = 0;
 
-		bool getIsLight ();
-		void setIsLight (bool isLight);
+    bool getIsLight ();
+    void setIsLight (bool isLight);
 
-		Color getColor ();
-		void setColor (Color color);
+    Color getColor ();
+    void setColor (Color color);
 
-	private:
-		bool _isLight;
-		Color _color;
+    private:
+    bool _isLight;
+    Color _color;
 };
 
 #endif
