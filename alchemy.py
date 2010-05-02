adc698bb43f292d4ba979d5b8748609b01585d1d
a_m0d <damien.schoof@gmail.com>
2010-05-02 23:52:17 +0800

    Changed alchemy api slightly

--- alchemy.py
+++ alchemy.py
@@ -1,6 +1,7 @@
 from buildit import *
 
 raytracer = Project("Raytracer")
+raytracer.require_lib('lib_jpeg')
 with raytracer.add_target(Executable()) as executable:
-    executable.printout()
+    executable.add(['.', 'math', 'graphics', 'primitives'])
 raytracer.run()
