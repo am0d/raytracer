9a83b30e7245b7f8ef0a5a5b58a331c8478baa46
a_m0d <damien.schoof@gmail.com>
2011-10-05 19:54:57 -0400

    Add GPL v3.0 license statement to top of each file.

--- src/ray3d.cpp
+++ src/ray3d.cpp
@@ -1,6 +1,18 @@
-/* Author: a_m0d <a_m0d@hotmail.com>
- * Date: May 2008
- * License: GPL version 2.0 or later
+/* Author: Damien Schoof
+ * Date created: June 2008
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "ray3d.hpp"
